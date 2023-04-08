b2be9450a9265340b6de84d6686467427e05091e
Rachit <rachit.kh4@gmail.com>
2023-04-09 02:56:53 +0530

    token check added

--- core/core/apis/routes/pmr_router.py
+++ core/core/apis/routes/pmr_router.py
@@ -3,6 +3,7 @@ from fastapi.security import OAuth2PasswordBearer
 from core.apis.schemas.requests.pmr_request import PMR
 from core.controllers.pmr_controller import PMRController
 from core import logger
+from commons.auth import decodeJWT
 
 logging = logger(__name__)
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/v1/user/signIn")
@@ -14,7 +15,15 @@ def create_pmr(pmr_request: PMR, token: str = Depends(oauth2_scheme)):
     try:
         logging.info("Calling /v1/pmr/create endpoint")
         logging.debug(f"Request: {pmr_request}")
-        return PMRController().create_pmr_controller(request=pmr_request)
+        authenticated_user_details = decodeJWT(token=token)
+        if authenticated_user_details:
+            return PMRController().create_pmr_controller(request=pmr_request)
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid access token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
     except HTTPException as httperror:
         logging.error(f"Error in /v1/pmr/create endpoint: {httperror}")
         raise httperror
