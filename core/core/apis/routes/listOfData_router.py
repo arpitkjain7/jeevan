13f325c9b85c601ba0982fd93164b08c6cb4c24a
Rachit <rachit.kh4@gmail.com>
2023-04-11 00:26:35 +0530

    adding all routers and controllers.

---
+++ core/core/apis/routes/listOfData_router.py
@@ -0,0 +1,447 @@
+from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordBearer
+
+from core.controllers.listOfComplaint_controller import ListOfComplaintsController
+from core.controllers.listOfDiagnosis_controller import ListOfDiagnosisController
+from core.controllers.listOfMedicalTests_controller import ListOfMedicalTestsController
+from core.controllers.listOfMedicines_controller import ListOfMedicinesController
+from commons.auth import decodeJWT
+from fastapi.security import OAuth2PasswordRequestForm
+from core import logger
+
+logging = logger(__name__)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/v1/user/signIn")
+listOfData_router = APIRouter()
+
+"""listOfComplaint_router = APIRouter()
+listOfDiagnosis_router = APIRouter()
+listOfMedicalTests_router = APIRouter()
+listOfMedicines_router = APIRouter()
+"""
+
+
+@listOfData_router.post("/v1/listOfComplaints/create")
+def add_complaint(complaint: str, token: str = Depends(oauth2_scheme)):
+    """[API router to register new user into the system]
+    Args:
+        register_user_request (Register): [New user details]
+    Raises:
+        HTTPException: [Unauthorized exception when invalid token is passed]
+        error: [Exception in underlying controller]
+    Returns:
+        [RegisterResponse]: [Register new user response]
+    """
+    try:
+        logging.info("Calling /v1/listOfComplaints/create")
+        logging.debug(f"Request: {complaint}")
+        authenticated_user_details = decodeJWT(token=token)
+        if authenticated_user_details:
+            return ListOfComplaintsController().add_complaint_controller(complaint)
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid access token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except HTTPException as httperror:
+        logging.error(f"Error in /v1/listOfComplaints/create endpoint: {httperror}")
+        raise httperror
+    except Exception as error:
+        logging.error(f"Error in /v1/listOfComplaints/create endpoint: {error}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(error),
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+@listOfData_router.get("/v1/listOfComplaints/getAll")
+def get_all_complaint(token: str = Depends(oauth2_scheme)):
+    """[Get List of all Users]
+    Raises:
+        error: [Error details]
+    Returns:
+        [list]: [List of Users]
+    """
+    try:
+        logging.info("Calling /v1/listOfComplaints/getAll endpoint")
+        authenticated_user_details = decodeJWT(token=token)
+        if authenticated_user_details:
+            return ListOfComplaintsController().get_all_complaints_controller()
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid access token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except HTTPException as httperror:
+        logging.error(f"Error in /v1/listOfComplaints/getAll endpoint: {httperror}")
+        raise httperror
+    except Exception as error:
+        logging.error(f"Error in /v1/listOfComplaints/getAll endpoint: {error}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(error),
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+@listOfData_router.post("/v1/listOfComplaints/delete")
+def delete_complaint(complaint_id: int, token: str = Depends(oauth2_scheme)):
+    """[API router to register new user into the system]
+    Args:
+        register_user_request (Register): [New user details]
+    Raises:
+        HTTPException: [Unauthorized exception when invalid token is passed]
+        error: [Exception in underlying controller]
+    Returns:
+        [RegisterResponse]: [Register new user response]
+    """
+    try:
+        logging.info("Calling /v1/listOfComplaints/delete")
+        logging.debug(f"Request: {complaint_id}")
+        authenticated_user_details = decodeJWT(token=token)
+        if authenticated_user_details:
+            return ListOfComplaintsController().delete_complaint_controller(
+                complaint_id
+            )
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid access token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except HTTPException as httperror:
+        logging.error(f"Error in /v1/listOfComplaints/delete endpoint: {httperror}")
+        raise httperror
+    except Exception as error:
+        logging.error(f"Error in /v1/listOfComplaints/delete endpoint: {error}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(error),
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+@listOfData_router.post("/v1/listOfDiagnosis/addDisease")
+def add_disease(disease: str, token: str = Depends(oauth2_scheme)):
+    """[API router to register new user into the system]
+    Args:
+        register_user_request (Register): [New user details]
+    Raises:
+        HTTPException: [Unauthorized exception when invalid token is passed]
+        error: [Exception in underlying controller]
+    Returns:
+        [RegisterResponse]: [Register new user response]
+    """
+    try:
+        logging.info("Calling /v1/listOfComplaints/create")
+        logging.debug(f"Request: {disease}")
+        authenticated_user_details = decodeJWT(token=token)
+        if authenticated_user_details:
+            return ListOfDiagnosisController().add_disease_controller(disease)
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid access token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except HTTPException as httperror:
+        logging.error(f"Error in /v1/listOfDiagnosis/create endpoint: {httperror}")
+        raise httperror
+    except Exception as error:
+        logging.error(f"Error in /v1/listOfDiagnosis/create endpoint: {error}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(error),
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+@listOfData_router.get("/v1/listOfDiagnosis/getAllDiseases")
+def get_all_diseases(token: str = Depends(oauth2_scheme)):
+    """[Get List of all Users]
+    Raises:
+        error: [Error details]
+    Returns:
+        [list]: [List of Users]
+    """
+    try:
+        logging.info("Calling /v1/listOfDiagnosis/getAllDiseases endpoint")
+        authenticated_user_details = decodeJWT(token=token)
+        if authenticated_user_details:
+            return ListOfDiagnosisController().get_all_diseases_controller()
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid access token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except HTTPException as httperror:
+        logging.error(
+            f"Error in /v1/listOfDiagnosis/getAllDiseases endpoint: {httperror}"
+        )
+        raise httperror
+    except Exception as error:
+        logging.error(f"Error in /v1/listOfDiagnosis/getAllDiseases endpoint: {error}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(error),
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+@listOfData_router.post("/v1/listOfDiagnosis/deleteDisease")
+def delete_disease(disease_id: int, token: str = Depends(oauth2_scheme)):
+    """[API router to register new user into the system]
+    Args:
+        register_user_request (Register): [New user details]
+    Raises:
+        HTTPException: [Unauthorized exception when invalid token is passed]
+        error: [Exception in underlying controller]
+    Returns:
+        [RegisterResponse]: [Register new user response]
+    """
+    try:
+        logging.info("Calling /v1/listOfDiagnosis/deleteDisease")
+        logging.debug(f"Request: {disease_id}")
+        authenticated_user_details = decodeJWT(token=token)
+        if authenticated_user_details:
+            return ListOfDiagnosisController().delete_disease_controller(disease_id)
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid access token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except HTTPException as httperror:
+        logging.error(
+            f"Error in /v1/listOfDiagnosis/deleteDisease endpoint: {httperror}"
+        )
+        raise httperror
+    except Exception as error:
+        logging.error(f"Error in /v1/listOfDiagnosis/deleteDisease endpoint: {error}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(error),
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+@listOfData_router.post("/v1/listOfMedicalTests/addMedicalTest")
+def add_medical_test(medicalTest: str, token: str = Depends(oauth2_scheme)):
+    """[API router to register new user into the system]
+    Args:
+        register_user_request (Register): [New user details]
+    Raises:
+        HTTPException: [Unauthorized exception when invalid token is passed]
+        error: [Exception in underlying controller]
+    Returns:
+        [RegisterResponse]: [Register new user response]
+    """
+    try:
+        logging.info("Calling /v1/listOfMedicalTests/addMedicalTest")
+        logging.debug(f"Request: {medicalTest}")
+        authenticated_user_details = decodeJWT(token=token)
+        if authenticated_user_details:
+            return ListOfMedicalTestsController().add_medicalTest_controller(
+                medicalTest
+            )
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid access token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except HTTPException as httperror:
+        logging.error(
+            f"Error in /v1/listOfMedicalTests/addMedicalTest endpoint: {httperror}"
+        )
+        raise httperror
+    except Exception as error:
+        logging.error(
+            f"Error in /v1/listOfMedicalTests/addMedicalTest endpoint: {error}"
+        )
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(error),
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+@listOfData_router.get("/v1/listOfMedicalTest/getAllTests")
+def get_all_medical_tests(token: str = Depends(oauth2_scheme)):
+    """[Get List of all Users]
+    Raises:
+        error: [Error details]
+    Returns:
+        [list]: [List of Users]
+    """
+    try:
+        logging.info("Calling /v1/listOfMedicalTest/getAllTests endpoint")
+        authenticated_user_details = decodeJWT(token=token)
+        if authenticated_user_details:
+            return ListOfMedicalTestsController().get_all_tests_controller()
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid access token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except HTTPException as httperror:
+        logging.error(
+            f"Error in /v1/listOfMedicalTest/getAllTests endpoint: {httperror}"
+        )
+        raise httperror
+    except Exception as error:
+        logging.error(f"Error in /v1/listOfMedicalTest/getAllTests endpoint: {error}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(error),
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+@listOfData_router.post("/v1/listOfMedicalTest/deleteTest")
+def delete_medical_test(test_id: int, token: str = Depends(oauth2_scheme)):
+    """[API router to register new user into the system]
+    Args:
+        register_user_request (Register): [New user details]
+    Raises:
+        HTTPException: [Unauthorized exception when invalid token is passed]
+        error: [Exception in underlying controller]
+    Returns:
+        [RegisterResponse]: [Register new user response]
+    """
+    try:
+        logging.info("Calling /v1/listOfMedicalTest/deleteTest")
+        logging.debug(f"Request: {test_id}")
+        authenticated_user_details = decodeJWT(token=token)
+        if authenticated_user_details:
+            return ListOfMedicalTestsController().delete_test_controller(test_id)
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid access token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except HTTPException as httperror:
+        logging.error(
+            f"Error in /v1/listOfMedicalTest/deleteTest endpoint: {httperror}"
+        )
+        raise httperror
+    except Exception as error:
+        logging.error(f"Error in /v1/listOfMedicalTest/deleteTest endpoint: {error}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(error),
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+@listOfData_router.post("/v1/listOfMedicines/addMedicine")
+def add_medicine(name: str, company: str, token: str = Depends(oauth2_scheme)):
+    """[API router to register new user into the system]
+    Args:
+        register_user_request (Register): [New user details]
+    Raises:
+        HTTPException: [Unauthorized exception when invalid token is passed]
+        error: [Exception in underlying controller]
+    Returns:
+        [RegisterResponse]: [Register new user response]
+    """
+    try:
+        logging.info("Calling /v1/listOfMedicine/addMedicine")
+        logging.debug(f"Request: {name}")
+        authenticated_user_details = decodeJWT(token=token)
+        if authenticated_user_details:
+            return ListOfMedicinesController().add_medicine_controller(
+                name,
+                company,
+            )
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid access token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except HTTPException as httperror:
+        logging.error(f"Error in /v1/listOfMedicines/addMedicine endpoint: {httperror}")
+        raise httperror
+    except Exception as error:
+        logging.error(f"Error in /v1/listOfMedicines/addMedicine endpoint: {error}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(error),
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+@listOfData_router.get("/v1/listOfMedicines/getAllMedicines")
+def get_all_medicines(token: str = Depends(oauth2_scheme)):
+    """[Get List of all Users]
+    Raises:
+        error: [Error details]
+    Returns:
+        [list]: [List of Users]
+    """
+    try:
+        logging.info("Calling /v1/listOfMedicines/getAllMedicines endpoint")
+        authenticated_user_details = decodeJWT(token=token)
+        if authenticated_user_details:
+            return ListOfMedicinesController().get_all_medicines_controller()
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid access token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except HTTPException as httperror:
+        logging.error(
+            f"Error in /v1/listOfMedicines/getAllMedicines endpoint: {httperror}"
+        )
+        raise httperror
+    except Exception as error:
+        logging.error(f"Error in /v1/listOfMedicines/getAllMedicines endpoint: {error}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(error),
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+@listOfData_router.post("/v1/listOfMedicines/deleteMedicine")
+def delete_medicine(medicine_id: int, token: str = Depends(oauth2_scheme)):
+    """[API router to register new user into the system]
+    Args:
+        register_user_request (Register): [New user details]
+    Raises:
+        HTTPException: [Unauthorized exception when invalid token is passed]
+        error: [Exception in underlying controller]
+    Returns:
+        [RegisterResponse]: [Register new user response]
+    """
+    try:
+        logging.info("Calling /v1/listOfMedicines/deleteMedicine")
+        logging.debug(f"Request: {medicine_id}")
+        authenticated_user_details = decodeJWT(token=token)
+        if authenticated_user_details:
+            return ListOfMedicinesController().delete_medicine_controller(medicine_id)
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid access token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except HTTPException as httperror:
+        logging.error(
+            f"Error in /v1/listOfMedicines/deleteMedicine endpoint: {httperror}"
+        )
+        raise httperror
+    except Exception as error:
+        logging.error(f"Error in /v1/listOfMedicines/deleteMedicine endpoint: {error}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(error),
+            headers={"WWW-Authenticate": "Bearer"},
+        )
