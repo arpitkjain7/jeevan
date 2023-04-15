e35cbdb17b4a7459a025fecc62403737c512354a
Arpit Jain <arpitjain101992@gmail.com>
2023-04-15 21:15:24 +0530

    request schema added

--- core/core/apis/schemas/requests/hid_request.py
+++ core/core/apis/schemas/requests/hid_request.py
@@ -11,3 +11,21 @@ class AbhaRegistration(BaseModel):
     healthId: str
     password: str
     txnId: str
+
+
+class OTPVerification(BaseModel):
+    otp: str
+    txn_id: str
+
+
+class MobileOTP(BaseModel):
+    mobile_number: str
+    txn_id: str
+
+
+class AadhaarNumber(BaseModel):
+    aadhaar_number: str
+
+
+class HealthNumber(BaseModel):
+    health_number: str
