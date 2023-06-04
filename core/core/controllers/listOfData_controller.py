5162910e2526e617fa359da80179190763aad6a9
Rachit <rachit.kh4@gmail.com>
2023-06-04 16:41:41 +0530

    deeplink

--- core/core/controllers/listOfData_controller.py
+++ core/core/controllers/listOfData_controller.py
@@ -371,6 +371,7 @@ class Common:
     def __init__(self):
         self.abha_url = os.environ["abha_url"]
         self.s3_location = os.environ["s3_location"]
+        self.gateway_url = os.environ["gateway_url"]
 
     def abha_availability(self, health_id: str):
         """Verify if the abha address already exists
@@ -407,4 +408,53 @@ class Common:
                 )
         except Exception as error:
             logging.error(f"Error in HIDController.abha_availability function: {error}")
+            raise error
+
+    def deep_link_notify(self, mobile_no: str):
+        """Verify if the abha address already exists
+        Args:
+            health_id (str): abha address to be checked
+        Raises:
+            HTTPException: _description_
+            error: _description_
+        Returns:
+            _type_: _description_
+        """
+        try:
+            logging.info("executing  deep_link_notify function")
+            gateway_access_token = get_session_token(
+                session_parameter="gateway_token"
+            ).get("accessToken")
+            deep_link_url = f"{self.gateway_url }/v0.5/patients/sms/notify"
+            resp, resp_code = APIInterface().post(
+                route=deep_link_url,
+                data={
+                    {
+                        "requestId": "{{$guid}}",
+                        "timestamp": "2023-05-27T08:41:55Z",
+                        "notification": {
+                            "phoneNo": "+91-9511878113",
+                            "hip": {
+                                "name": "Max Healthcare",
+                                "id": "ABCABC",
+                            },
+                        },
+                    }
+                },
+                headers={"Authorization": f"Bearer {gateway_access_token}"},
+            )
+            available_status = resp.get("status")
+            logging.info(f"{available_status=}")
+            if resp_code <= 250:
+                if resp.get("status") == True:
+                    return {"available": False}
+                return {"available": True}
+            else:
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail=resp,
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+        except Exception as error:
+            logging.error(f"Error in HIDController.deep_link_notify function: {error}")
             raise error
