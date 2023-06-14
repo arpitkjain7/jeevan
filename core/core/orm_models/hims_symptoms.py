6bea4022f83c18bef4e1abe38b42fef0f087d229
Rachit <rachit.kh4@gmail.com>
2023-06-14 19:39:18 +0530

    Db updates

--- core/core/orm_models/hims_symptoms.py
+++ core/core/orm_models/hims_symptoms.py
@@ -1,18 +1,16 @@
-from sqlalchemy import Column, String, Integer, ForeignKey, DateTime, Text
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from core import Base
 
 
-class Medicines(Base):
-    __tablename__ = "medicines"
+class Symptoms(Base):
+    __tablename__ = "symptoms"
     __table_args__ = {"extend_existing": True}
     id = Column(Integer, primary_key=True, autoincrement=True)
     pmr_id = Column(String, ForeignKey("patientMedicalRecord.id"))
-    medicine_name = Column(String)
-    dosage = Column(String)
-    frequency = Column(String)
-    time_of_day = Column(String)
+    symptom = Column(String)
     duration = Column(String)
-    duration_period = Column(String)
+    severity = Column(String)
     notes = Column(String)
+    start_date = Column(DateTime)
     created_at = Column(DateTime)
     updated_at = Column(DateTime)
