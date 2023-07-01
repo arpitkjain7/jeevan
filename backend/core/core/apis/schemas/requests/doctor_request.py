849d4f6c6c01d057eb6dc8d4afc05fec9f430e0d
Rachit <rachit.kh4@gmail.com>
2023-07-01 16:06:54 +0530

    changes made

--- backend/core/core/apis/schemas/requests/doctor_request.py
+++ backend/core/core/apis/schemas/requests/doctor_request.py
@@ -8,8 +8,8 @@ class UpdateDoctor(BaseModel):
     doc_name: str
     doc_working_days: str
     avg_consultation_time: str
-    start_time: time
-    end_time: time
+    consultation_start_time: time
+    consultation_end_time: time
     consultation_fees: int
     follow_up_fees: int
     hip_id: str
