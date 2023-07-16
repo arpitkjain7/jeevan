73713decd1c91fd6e1d75a1b219f0f8f5b8a5795
Arpit Jain <arpitjain101992@gmail.com>
2023-07-16 10:09:09 +0530

    bugfix

--- backend/core/core/controllers/hid_controller.py
+++ backend/core/core/controllers/hid_controller.py
@@ -570,7 +570,7 @@ class HIDController:
             txn_id = request_json.get("txnId")
             dob = request_json.get("dob")
             date, month, year = dob.split("/")
-            name = request_json.get("firstName") + request_json.get("lastName")
+            name = f"{request_json.get('firstName')} {request_json.get('lastName')}"
             token = self.CRUDGatewayInteraction.read(request_id=txn_id).get("token")
 
             request_json.update(
