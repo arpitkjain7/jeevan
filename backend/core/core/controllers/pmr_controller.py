03ad2fdda0de9310b871ec892405d5c7f331de9c
Arpit Jain <arpitjain101992@gmail.com>
2023-10-22 12:50:09 +0530

    abdm linked flag update step added

--- backend/core/core/controllers/pmr_controller.py
+++ backend/core/core/controllers/pmr_controller.py
@@ -568,6 +568,7 @@ class PMRController:
                         "Authorization": f"Bearer {gateway_access_token}",
                     },
                 )
+
                 self.CRUDGatewayInteraction.create(
                     **{
                         "request_id": request_id,
@@ -580,6 +581,11 @@ class PMRController:
                         },
                     }
                 )
+                if not pmr_obj["abdm_linked"]:
+                    logging.info("Updating abdm linked flag on PMR")
+                    self.CRUDPatientMedicalRecord.update(
+                        pmr_id=pmr_id, **{"abdm_linked": True}
+                    )
                 logging.info("Sending SMS notification")
                 sms_notify_url = f"{self.gateway_url}/v0.5/patients/sms/notify"
                 mobile_number = patient_obj.get("mobile_number")
