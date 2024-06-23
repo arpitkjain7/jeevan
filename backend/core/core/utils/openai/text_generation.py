df4f386d9761180432bedc9483983305d3e12324
Arpit Jain <arpitjain101992@gmail.com>
2024-06-23 12:01:54 +0530

    whisper APIs added

---
+++ backend/core/core/utils/openai/text_generation.py
@@ -0,0 +1,45 @@
+import json
+import os
+import openai
+
+
+class TextGeneration:
+    def __init__(self):
+        self.api_key = os.environ.get("OPENAI_API_KEY")
+        self.model_name = os.environ.get("OPENAI_MODEL")
+        self.temperature = 0.0
+        self.max_tokens = 2000
+
+        if self.api_key:
+            openai.api_key = self.api_key
+
+    def send_completion_request(
+        self, prompt: str, chat: bool = False, stream: bool = False
+    ) -> dict:
+        response = {}
+        if chat:
+            response = openai.chat.completions.create(
+                model=self.model_name,
+                messages=[
+                    {"role": "system", "content": prompt},
+                    {
+                        "role": "user",
+                        "content": "Ensure the report is comprehensive, structured, and strictly adhere to the report Structure. Patient Report json:",
+                    },
+                ],
+                response_format={"type": "json_object"},
+                temperature=self.temperature,
+                max_tokens=self.max_tokens,
+                stream=stream,
+            )
+            response = response.choices[0].message.content
+        else:
+            response = openai.completions.create(
+                model=self.model_name,
+                prompt=f"""{prompt}. \nEnsure the report is comprehensive, structured, and strictly adhere to the report Structure. Patient Report json:""",
+                temperature=self.temperature,
+                max_tokens=self.max_tokens,
+                stream=stream,
+            )
+            response = response.choices[0].text
+        return json.loads(response)
